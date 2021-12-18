d9d149b817e6c277f699e6c300435ede8986a641
Callum <callumnewsome100@gmail.com>
2021-12-18 13:34:19 +0000

    Add project files.

---
+++ SimpleSpecs.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace SimpleSpecs
+{
+    partial class SimpleSpecs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new MetroFramework.Controls.MetroLabel();
+            this.advancedModeToggle = new MetroFramework.Controls.MetroToggle();
+            this.closeLabel = new MetroFramework.Controls.MetroLabel();
+            this.cpuSectionLabel = new MetroFramework.Controls.MetroLabel();
+            this.cpuLabel = new MetroFramework.Controls.MetroLabel();
+            this.advancedModeLabel = new MetroFramework.Controls.MetroLabel();
+            this.gpuLabel = new MetroFramework.Controls.MetroLabel();
+            this.memoryLabel = new MetroFramework.Controls.MetroLabel();
+            this.storageLabel = new MetroFramework.Controls.MetroLabel();
+            this.gpuSectionLabel = new MetroFramework.Controls.MetroLabel();
+            this.memorySectionLabel = new MetroFramework.Controls.MetroLabel();
+            this.storageSectionLabel = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.titleLabel.CustomBackground = true;
+            this.titleLabel.CustomForeColor = true;
+            this.titleLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.titleLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.titleLabel.Location = new System.Drawing.Point(0, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(800, 40);
+            this.titleLabel.TabIndex = 2;
+            this.titleLabel.Text = "Simple Specs";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.titleLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.titleLabel_MouseDown);
+            this.titleLabel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.titleLabel_MouseMove);
+            this.titleLabel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.titleLabel_MouseUp);
+            // 
+            // advancedModeToggle
+            // 
+            this.advancedModeToggle.AutoSize = true;
+            this.advancedModeToggle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.advancedModeToggle.CustomBackground = true;
+            this.advancedModeToggle.Location = new System.Drawing.Point(412, 69);
+            this.advancedModeToggle.Name = "advancedModeToggle";
+            this.advancedModeToggle.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.advancedModeToggle.Size = new System.Drawing.Size(80, 17);
+            this.advancedModeToggle.TabIndex = 2;
+            this.advancedModeToggle.Text = "Off";
+            this.advancedModeToggle.UseVisualStyleBackColor = false;
+            this.advancedModeToggle.CheckedChanged += new System.EventHandler(this.advancedModeToggle_CheckedChanged);
+            // 
+            // closeLabel
+            // 
+            this.closeLabel.BackColor = System.Drawing.Color.Red;
+            this.closeLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeLabel.CustomBackground = true;
+            this.closeLabel.CustomForeColor = true;
+            this.closeLabel.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.closeLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.closeLabel.Location = new System.Drawing.Point(763, 8);
+            this.closeLabel.Name = "closeLabel";
+            this.closeLabel.Size = new System.Drawing.Size(25, 25);
+            this.closeLabel.TabIndex = 4;
+            this.closeLabel.Text = "X";
+            this.closeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.closeLabel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.closeLabel_MouseClick);
+            this.closeLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.closeLabel_MouseDown);
+            this.closeLabel.MouseLeave += new System.EventHandler(this.closeLabel_MouseLeave);
+            this.closeLabel.MouseHover += new System.EventHandler(this.closeLabel_MouseHover);
+            this.closeLabel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.closeLabel_MouseUp);
+            // 
+            // cpuSectionLabel
+            // 
+            this.cpuSectionLabel.CustomBackground = true;
+            this.cpuSectionLabel.CustomForeColor = true;
+            this.cpuSectionLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.cpuSectionLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.cpuSectionLabel.Location = new System.Drawing.Point(12, 103);
+            this.cpuSectionLabel.Name = "cpuSectionLabel";
+            this.cpuSectionLabel.Size = new System.Drawing.Size(142, 40);
+            this.cpuSectionLabel.TabIndex = 6;
+            this.cpuSectionLabel.Text = "CPU";
+            this.cpuSectionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cpuLabel
+            // 
+            this.cpuLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cpuLabel.CustomBackground = true;
+            this.cpuLabel.CustomForeColor = true;
+            this.cpuLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.cpuLabel.Location = new System.Drawing.Point(12, 143);
+            this.cpuLabel.Name = "cpuLabel";
+            this.cpuLabel.Size = new System.Drawing.Size(375, 120);
+            this.cpuLabel.TabIndex = 7;
+            this.cpuLabel.Text = "Loading...";
+            this.cpuLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // advancedModeLabel
+            // 
+            this.advancedModeLabel.CustomBackground = true;
+            this.advancedModeLabel.CustomForeColor = true;
+            this.advancedModeLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.advancedModeLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.advancedModeLabel.Location = new System.Drawing.Point(270, 57);
+            this.advancedModeLabel.Name = "advancedModeLabel";
+            this.advancedModeLabel.Size = new System.Drawing.Size(222, 40);
+            this.advancedModeLabel.TabIndex = 8;
+            this.advancedModeLabel.Text = "Advanced Mode";
+            this.advancedModeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // gpuLabel
+            // 
+            this.gpuLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gpuLabel.CustomBackground = true;
+            this.gpuLabel.CustomForeColor = true;
+            this.gpuLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.gpuLabel.Location = new System.Drawing.Point(413, 143);
+            this.gpuLabel.Name = "gpuLabel";
+            this.gpuLabel.Size = new System.Drawing.Size(375, 120);
+            this.gpuLabel.TabIndex = 9;
+            this.gpuLabel.Text = "Loading...";
+            this.gpuLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // memoryLabel
+            // 
+            this.memoryLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.memoryLabel.CustomBackground = true;
+            this.memoryLabel.CustomForeColor = true;
+            this.memoryLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.memoryLabel.Location = new System.Drawing.Point(12, 318);
+            this.memoryLabel.Name = "memoryLabel";
+            this.memoryLabel.Size = new System.Drawing.Size(375, 120);
+            this.memoryLabel.TabIndex = 10;
+            this.memoryLabel.Text = "Loading...";
+            this.memoryLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // storageLabel
+            // 
+            this.storageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.storageLabel.CustomBackground = true;
+            this.storageLabel.CustomForeColor = true;
+            this.storageLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.storageLabel.Location = new System.Drawing.Point(413, 318);
+            this.storageLabel.Name = "storageLabel";
+            this.storageLabel.Size = new System.Drawing.Size(375, 120);
+            this.storageLabel.TabIndex = 11;
+            this.storageLabel.Text = "Loading...";
+            this.storageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // gpuSectionLabel
+            // 
+            this.gpuSectionLabel.CustomBackground = true;
+            this.gpuSectionLabel.CustomForeColor = true;
+            this.gpuSectionLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.gpuSectionLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.gpuSectionLabel.Location = new System.Drawing.Point(413, 103);
+            this.gpuSectionLabel.Name = "gpuSectionLabel";
+            this.gpuSectionLabel.Size = new System.Drawing.Size(142, 40);
+            this.gpuSectionLabel.TabIndex = 12;
+            this.gpuSectionLabel.Text = "GPU";
+            this.gpuSectionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // memorySectionLabel
+            // 
+            this.memorySectionLabel.CustomBackground = true;
+            this.memorySectionLabel.CustomForeColor = true;
+            this.memorySectionLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.memorySectionLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.memorySectionLabel.Location = new System.Drawing.Point(12, 278);
+            this.memorySectionLabel.Name = "memorySectionLabel";
+            this.memorySectionLabel.Size = new System.Drawing.Size(142, 40);
+            this.memorySectionLabel.TabIndex = 13;
+            this.memorySectionLabel.Text = "Memory";
+            this.memorySectionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // storageSectionLabel
+            // 
+            this.storageSectionLabel.CustomBackground = true;
+            this.storageSectionLabel.CustomForeColor = true;
+            this.storageSectionLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.storageSectionLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.storageSectionLabel.Location = new System.Drawing.Point(413, 278);
+            this.storageSectionLabel.Name = "storageSectionLabel";
+            this.storageSectionLabel.Size = new System.Drawing.Size(142, 40);
+            this.storageSectionLabel.TabIndex = 14;
+            this.storageSectionLabel.Text = "Storage";
+            this.storageSectionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // SimpleSpecs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.storageSectionLabel);
+            this.Controls.Add(this.memorySectionLabel);
+            this.Controls.Add(this.gpuSectionLabel);
+            this.Controls.Add(this.storageLabel);
+            this.Controls.Add(this.memoryLabel);
+            this.Controls.Add(this.gpuLabel);
+            this.Controls.Add(this.advancedModeToggle);
+            this.Controls.Add(this.advancedModeLabel);
+            this.Controls.Add(this.cpuLabel);
+            this.Controls.Add(this.cpuSectionLabel);
+            this.Controls.Add(this.closeLabel);
+            this.Controls.Add(this.titleLabel);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "SimpleSpecs";
+            this.Text = "Simple Specs";
+            this.Load += new System.EventHandler(this.SimpleSpecs_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroLabel titleLabel;
+        private MetroFramework.Controls.MetroToggle advancedModeToggle;
+        private MetroFramework.Controls.MetroLabel closeLabel;
+        private MetroFramework.Controls.MetroLabel cpuSectionLabel;
+        private MetroFramework.Controls.MetroLabel cpuLabel;
+        private MetroFramework.Controls.MetroLabel advancedModeLabel;
+        private MetroFramework.Controls.MetroLabel gpuLabel;
+        private MetroFramework.Controls.MetroLabel memoryLabel;
+        private MetroFramework.Controls.MetroLabel storageLabel;
+        private MetroFramework.Controls.MetroLabel gpuSectionLabel;
+        private MetroFramework.Controls.MetroLabel memorySectionLabel;
+        private MetroFramework.Controls.MetroLabel storageSectionLabel;
+    }
+}
+
---
+++ SimpleSpecs.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleSpecs
+{
+    public partial class SimpleSpecs : Form
+    {
+        private bool mouseDown;
+        private Point lastLocation;
+
+        public SimpleSpecs()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void titleLabel_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void titleLabel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void titleLabel_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+
+
+        private void closeLabel_MouseClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void closeLabel_MouseHover(object sender, EventArgs e)
+        {
+            closeLabel.BackColor = Color.FromArgb(227, 0, 0);
+        }
+
+        private void closeLabel_MouseLeave(object sender, EventArgs e)
+        {
+            closeLabel.BackColor = Color.FromArgb(255, 0, 0);
+        }
+
+        private void closeLabel_MouseDown(object sender, MouseEventArgs e)
+        {
+            closeLabel.BackColor = Color.FromArgb(200, 0, 0);
+        }
+
+        private void closeLabel_MouseUp(object sender, MouseEventArgs e)
+        {
+            closeLabel.BackColor = Color.FromArgb(255, 0, 0);
+        }
+
+
+
+        private void advancedModeToggle_CheckedChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("changed");
+        }
+
+
+
+        private List<PropertyData> GetInfo(string query)
+        {
+            List<PropertyData> info = new List<PropertyData>();
+
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(String.Format("SELECT * FROM {0}", query));
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    PropertyDataCollection props = obj.Properties;
+                    foreach (PropertyData data in props)
+                    {
+                        info.Add(data);
+                    }
+                }
+            }
+            catch (Exception ex)
+            { }
+
+            return info;
+        }
+
+        private List<string> GetCPU()
+        {
+            /**
+             * CPU
+             */
+            List<PropertyData> processorInfo = GetInfo("Win32_Processor");
+            List<string> cpu = new List<string>();
+
+            // Name
+            object name = processorInfo.Find(i => i.Name == "Name").Value;
+            cpu.Add(String.Format("Name: {0}", name));
+
+            // Cores
+            object cores = processorInfo.Find(i => i.Name == "NumberOfCores").Value;
+            cpu.Add(String.Format("Cores: {0}", cores));
+
+            // Logical Processors
+            object logicalProcessors = processorInfo.Find(i => i.Name == "NumberOfLogicalProcessors").Value;
+            cpu.Add(String.Format("Logical Processors: {0}", logicalProcessors));
+
+            // Clock Speed
+            object clockSpeed = processorInfo.Find(i => i.Name == "CurrentClockSpeed").Value;
+            cpu.Add(String.Format("Clock Speed: {0} GHz", 0.001f * int.Parse(clockSpeed.ToString())));
+
+
+            return cpu;
+        }
+
+        private List<string> GetGPU()
+        {
+            /**
+             * GPU
+             */
+            List<PropertyData> videoControllerInfo = GetInfo("Win32_VideoController");
+            List<string> gpu = new List<string>();
+
+            // Name
+            object name = videoControllerInfo.Find(i => i.Name == "Name").Value;
+            gpu.Add($"Name: {name}");
+
+            // Resolution
+            object horizontalResolution = videoControllerInfo.Find(i => i.Name == "CurrentHorizontalResolution").Value;
+            object verticalResolution = videoControllerInfo.Find(i => i.Name == "CurrentVerticalResolution").Value;
+            gpu.Add($"Resolution: {horizontalResolution}x{verticalResolution}");
+
+            // Driver Version
+            object driverVersion = videoControllerInfo.Find(i => i.Name == "DriverVersion").Value;
+            gpu.Add($"Driver Version: {driverVersion}");
+
+            // Driver Date
+            string driverDate = videoControllerInfo.Find(i => i.Name == "DriverDate").Value.ToString();
+            string year = driverDate.Substring(0, 4);
+            string month = driverDate.Substring(4, 2);
+            string day = driverDate.Substring(6, 2);
+            gpu.Add($"Driver Date: {day}/{month}/{year}");
+
+
+            return gpu;
+        }
+
+        private string MemFormat(object kb)
+        {
+            Int64 gb = Int64.Parse(kb.ToString()) / 1000 / 1000;
+            if (gb >= 1000)
+            {
+                return $"{gb / 1000} TB";
+            }
+
+            return $"{gb} GB";
+        }
+
+        private List<string> GetMemory()
+        {
+            /**
+             * Memory
+             */
+            List<PropertyData> videoControllerInfo = GetInfo("Win32_OperatingSystem");
+            List<string> memory = new List<string>();
+
+            // Total Visible Memory
+            object totalVisibleMemory = videoControllerInfo.Find(i => i.Name == "TotalVisibleMemorySize").Value;
+            memory.Add($"Total Visible Memory: {MemFormat(totalVisibleMemory)}");
+
+            // Free Physical Memory
+            object freePhysicalMemory = videoControllerInfo.Find(i => i.Name == "FreePhysicalMemory").Value;
+            memory.Add($"Free Physical Memory: {MemFormat(freePhysicalMemory)}");
+
+            // Total Virtual Memory Size
+            object totalVirtualMemorySize = videoControllerInfo.Find(i => i.Name == "TotalVirtualMemorySize").Value;
+            memory.Add($"Total Virtual Memory Size: {MemFormat(totalVisibleMemory)}");
+
+            // Free Virtual Memory
+            object freeVirtualMemory = videoControllerInfo.Find(i => i.Name == "FreeVirtualMemory").Value;
+            memory.Add($"Free Virtual Memory: {MemFormat(totalVisibleMemory)}");
+
+
+            return memory;
+        }
+
+        private List<string> GetStorage()
+        {
+            /**
+             * Storage
+             */
+            System.IO.DriveInfo[] drives = System.IO.DriveInfo.GetDrives();
+            List<string> storage = new List<string>();
+
+            // Drive Count
+            int driveCount = drives.Length;
+            storage.Add($"Drive Count: {driveCount}");
+
+            // Total Storage
+            Int64 totalStorage = drives.Select(x => x.TotalSize).Aggregate((x, y) => x + y);
+            storage.Add($"Total Storage: {MemFormat(totalStorage / 1000)}");
+
+            // Free Storage
+            Int64 freeStorage = drives.Select(x => x.TotalFreeSpace).Aggregate((x, y) => x + y);
+            storage.Add($"Free Storage: {MemFormat(freeStorage / 1000)}");
+
+            // Used Storage
+            double usedStorage = ((double)(totalStorage - freeStorage) / totalStorage) * 100;
+            storage.Add($"Used Storage: {Math.Round(usedStorage, 0)}%");
+
+
+            return storage;
+        }
+
+        private void SimpleSpecs_Load(object sender, EventArgs e)
+        {
+            // Update CPU Info
+            List<string> cpu = GetCPU();
+            cpuLabel.Text = String.Join("\n", cpu);
+
+            // Update GPU Info
+            List<string> gpu = GetGPU();
+            gpuLabel.Text = String.Join("\n", gpu);
+
+            // Update Memory
+            List<string> memory = GetMemory();
+            memoryLabel.Text = String.Join("\n", memory);
+
+            // Update Storage
+            List<string> storage = GetStorage();
+            storageLabel.Text = String.Join("\n", storage);
+        }
+    }
+}
